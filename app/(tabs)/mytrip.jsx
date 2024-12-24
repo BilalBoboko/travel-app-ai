f91c04f077a3c286ca929b7b1ba722fa5b8c4066
BilalBoboko <apievbilal07@gmail.com>
2024-12-24 18:53:27 +0600

    first commit

--- app/(tabs)/mytrip.jsx
+++ app/(tabs)/mytrip.jsx
@@ -1,4 +1,4 @@
-import { View, Text, ActivityIndicator, ScrollView, TouchableOpacity } from 'react-native'
+  import { View, Text, ActivityIndicator, ScrollView, TouchableOpacity } from 'react-native'
 import React, { useEffect, useState } from 'react'
 import { Colors } from "./../../constants/Colors"
 import Ionicons from '@expo/vector-icons/Ionicons';
--- configs/FirebaseConfig.js
+++ configs/FirebaseConfig.js
@@ -6,11 +6,20 @@ import { getFirestore } from "firebase/firestore";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
+const googleGeminiApiKey = process.env.EXPO_PUBLIC_GOOGLE_GEMINI_API_KEY;
+print(googleGeminiApiKey)
+
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-    // GET YOUR CONFIG IN FIREBASE PLATFORM
-};
+    apiKey: "AIzaSyBwg4CY1u3Vyj6rHLg6YUM8AIEDQS6pefk",
+    authDomain: "ai-trawel-app.firebaseapp.com",
+    projectId: "ai-trawel-app",
+    storageBucket: "ai-trawel-app.firebasestorage.app",
+    messagingSenderId: "369593502164",
+    appId: "1:369593502164:web:afaa9fc34f8ad122336e41",
+    measurementId: "G-VK6L7X45ES"
+  };
 
 // Initialize Firebase
 export const app = initializeApp(firebaseConfig);
